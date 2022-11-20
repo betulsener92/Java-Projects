e1deeab227406df0d15eb4af7ac5afe36f83d6dd
Betul <betulsener92@gmail.com>
2022-11-20 15:00:09 +0300

    düzenlenecek

---
+++ src/main/java/atm_project/AccountRunner.java
@@ -0,0 +1,9 @@
+package atm_project;
+
+public class AccountRunner {
+    public static void main(String[] args) {
+        System.out.println("Hello, Welcome to The Last Code Benders ATM");
+
+
+    }
+}
